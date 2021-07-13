ffad56ffc19d562eb8e3b9df033b1ffdadd88eeb
mobin2006 <www.mahintorabiiimobin@gmail.com>
2021-07-13 11:07:04 +0430

    how to write the for program

---
+++ for.py
@@ -0,0 +1,20 @@
+# hello.my name is mobin.today i want to explain about a for and work whit for.
+# not at all.you are now thinking a for what is this.
+# okay. the for is loop that makes your work easy.
+# for example you want to get 100 number...
+# you cant 100 times write the [a=int(input("please enter a number...."))]...
+# no,no,no
+# you can write:
+# a = []
+# number = []
+# for a in range(100):
+#     a = int(input("please enter a number..."))
+#     print(a)
+
+
+a = [x**2 for x in range(1 , 100)]
+print(a)
+
+
+
+# finisf,goodby
