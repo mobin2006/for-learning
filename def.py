4d5ad4ce5ae691ad78301f2e02159da0947dedd1
mobin2006 <www.mahintorabiiimobin@gmail.com>
2021-07-14 17:23:19 +0430

    how to riwte def

---
+++ def.py
@@ -0,0 +1,22 @@
+# hello.
+# today i want to learn you the def.
+# not at all . what is def?
+# def is comman and you can write everything in def
+# for example:\
+# def hello():
+#     print("hello guys")
+
+
+
+
+def full_name(f_name , l_name , age):
+    name =[f_name , l_name , age]
+    return name
+
+name = full_name("mobin" , "mahintorbi" , "16")
+print(name) 
+
+
+
+
+
